aa86c856d3c90e4f4bff18fb726bb63cae4ea537
Nick Nicker <nick4nicker@gmail.com>
2025-12-21 23:08:21 +0100

    Properly protect IronGolem and CopperGolem (Golem) on fields

--- src/main/java/net/sacredlabyrinth/Phaed/PreciousStones/listeners/PSEntityListener.java
+++ src/main/java/net/sacredlabyrinth/Phaed/PreciousStones/listeners/PSEntityListener.java
@@ -542,7 +542,7 @@ public class PSEntityListener implements Listener {
             }
         }
 
-        if (event.getEntity() instanceof Ageable && !(event.getEntity() instanceof Monster)) {
+        if ( (event.getEntity() instanceof Ageable && !(event.getEntity() instanceof Monster)) || event.getEntity() instanceof Golem) {
             Field field = plugin.getForceFieldManager().getEnabledSourceField(event.getEntity().getLocation(), FieldFlag.PROTECT_ANIMALS);
 
             if (field != null) {
